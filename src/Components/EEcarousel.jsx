3eaf9f04933867fd986bb7631b72ff049ddcbfa1
Youssof-Kandil <yaskm2002@gmail.com>
2023-11-28 13:14:42 +0200

    carousel done for now

--- src/Components/EEcarousel.jsx
+++ src/Components/EEcarousel.jsx
@@ -7,8 +7,10 @@ import Logo from "../Assets/posh-logo-w-typeface.svg"
 import artist1 from "../Assets/artist-white.jpg"
 import Slider from 'react-slick'
 export default function EEcarousel() {
+
+  
   const settings = {
-    autoplay: true,
+    // autoplay: true,
     dots: true,
     infinite: true,
     speed: 500,
@@ -21,82 +23,94 @@ export default function EEcarousel() {
     <>
   <div className='carousel text-white'>
         <Slider {...settings}>
-          <div className='position-relative' >
-            <div className="layer-blur">
+          <div className='position-relative py-3' >
+            <div className="layer-blur bg1">
               <div className='layer'></div>
             </div>
-            <div className='d-flex py-5 justify-content-center align-items-center position-relative inner-carousel  '>
-              <div className='carousel-item-image me-5 rounded-3 overflow-hidden'>
+            <div className='row py-5 justify-content-center align-items-center position-relative inner-carousel  '>
+              <div className='carousel-item-image  rounded-3 overflow-hidden col-md-3'>
                 <img src={img1} alt="img1" className='w-100 h-100 inner-image' />
               </div>
-              <div className='carouel-item-data'>
+              <div className='carouel-item-data col-md-5'>
                 <h2 className=''>End Of Semester Party</h2>
                 <h5>Artistry</h5>
                 <h5>Sunday December 3, 9:00 AM</h5>
-                <div className='artist d-flex justify-content-center align-items-center'>
-                  <img src={artist1} alt="artistname" className='me-2' />
-                  <h6 className='fw-bold'>ZUEZUEU</h6>
-                </div>
-              </div> 
-            </div>
-            
-          </div>
-          <div className='position-relative' >
-            <div className="layer-blur">
-              <div className='layer'></div>
-            </div>
-            <div className='d-flex py-5 justify-content-center align-items-center position-relative inner-carousel  '>
-              <div className='carousel-item-image me-5 rounded-3 overflow-hidden'>
-                <img src={img1} alt="img1" className='w-100 h-100 inner-image' />
-              </div>
-              <div className='carouel-item-data'>
-                <h2>End Of Semester Party</h2>
-                <h5>Artistry</h5>
-                <h5>Sunday December 3, 9:00 AM</h5>
                 <div className='artist  rounded-5 d-flex justify-content-center align-items-center'>
                   <img src={artist1} alt="artistname" className='me-2' />
                   <h6 className='fw-bold'>ZUEZUEU</h6>
                 </div>
+                <button className="btn  bg-white  rounded-5" >
+                  Purchase Tickets
+                </button>
               </div> 
             </div>
             
           </div>
-          <div className='position-relative' >
-            <div className="layer-blur">
+          <div className='position-relative  py-3' >
+            <div className="layer-blur bg2">
               <div className='layer'></div>
             </div>
-            <div className='d-flex py-5 justify-content-center align-items-center position-relative inner-carousel  '>
-              <div className='carousel-item-image me-5 rounded-3 overflow-hidden'>
-                <img src={img1} alt="img1" className='w-100 h-100 inner-image' />
+            <div className=' py-5 position-relative inner-carousel row justify-content-center align-items-center'>
+              <div className='carousel-item-image rounded-3 overflow-hidden col-md-3'>
+                <img src={img2} alt="img1" className='w-100 h-100 inner-image' />
               </div>
-              <div className='carouel-item-data'>
-                <h2>End Of Semester Party</h2>
-                <h5>Artistry</h5>
-                <h5>Sunday December 3, 9:00 AM</h5>
+              <div className='carouel-item-data col-md-5'>
+                <h2>Miami Art Week Powered by Art Hearts Fashion Art Basel 2023</h2>
+                <h5>Fabel / Kiki on The River / Dream Rooftop / The Vessel</h5>
+                <h5>Friday December 8, 7:00 AM</h5>
                 <div className='artist  rounded-5 d-flex justify-content-center align-items-center'>
                   <img src={artist1} alt="artistname" className='me-2' />
                   <h6 className='fw-bold'>ZUEZUEU</h6>
                 </div>
+                <button className="btn  bg-white  rounded-5" >
+                  Purchase Tickets
+                </button>
               </div> 
             </div>
             
           </div>
-          <div className='position-relative' >
-            <div className="layer-blur">
+          <div className='position-relative  py-3' >
+            <div className="layer-blur bg3">
               <div className='layer'></div>
             </div>
-            <div className='d-flex py-5 justify-content-center align-items-center position-relative inner-carousel  '>
-              <div className='carousel-item-image me-5 rounded-3 overflow-hidden '>
-                <img src={img1} alt="img1" className='w-100 h-100 inner-image' />
+            <div className='row py-5 justify-content-center align-items-center position-relative inner-carousel  '>
+              <div className='carousel-item-image  rounded-3 overflow-hidden col-md-3'>
+                <img src={img3} alt="img1" className='w-100 h-100 inner-image' />
               </div>
-              <div className='carouel-item-data'>
-                <h2>End Of Semester Party</h2>
-                <h5>Artistry</h5>
-                <h5>Sunday December 3, 9:00 AM</h5>
+              <div className='carouel-item-data col-md-5'>
+                <h2>RUNAWAY NEW YORK PRESENTS: OPEN 'TIL MIDNIGHT</h2>
+                <h5>Public Records</h5>
+                <h5>Tuesday December 12, 2:00 AM</h5>
                 <div className='artist  rounded-5 d-flex justify-content-center align-items-center'>
                   <img src={artist1} alt="artistname" className='me-2' />
                   <h6 className='fw-bold'>ZUEZUEU</h6>
                 </div>
+                <button className="btn  bg-white  rounded-5" >
+                  Purchase Tickets
+                </button>
+              </div> 
+            </div>
+            
+          </div>
+          <div className='position-relative py-3'  >
+            <div className="layer-blur bg4">
+              <div className='layer'></div>
+            </div>
+            <div className='row py-5 justify-content-center align-items-center position-relative inner-carousel  '>
+              <div className='carousel-item-image  rounded-3 overflow-hidden col-md-4 '>
+                <img src={img4} alt="img1" className='w-100 h-100 inner-image' />
+              </div>
+              <div className='carouel-item-data col-md-5'>
+                <h2>New York Fashion Week Powered by Art Hearts Fashion</h2>
+                <h5>The Angel Orensanz Foundation</h5>
+                <h5>Friday February 9, 5:00 AM</h5>
+                <div className='artist  rounded-5 d-flex justify-content-center align-items-center'>
+                  <img src={artist1} alt="artistname" className='me-2' />
+                  <h6 className='fw-bold'>ZUEZUEU</h6>
+                </div>
+                <button className="btn  bg-white  rounded-5" >
+                  Purchase Tickets
+                </button>
               </div> 
             </div>
             
