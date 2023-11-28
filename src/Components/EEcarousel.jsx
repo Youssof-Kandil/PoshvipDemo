87b0b632a7709e53fe66b4f8d2eb97049b2bea79
Youssof-Kandil <yaskm2002@gmail.com>
2023-11-28 09:29:12 +0200

    Project_Setup

---
+++ src/Components/EEcarousel.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function EEcarousel() {
+  return (
+    <div>EEcarousel</div>
+  )
+}
---
+++ src/Components/ExploreEvents.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import EEcarousel from './EEcarousel'
+
+export default function ExploreEvents() {
+  return (
+    <>
+    <EEcarousel/>
+    </>
+  )
+}
